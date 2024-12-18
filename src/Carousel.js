1ee6f6e12607e94a7d249a33145c2f2a16817691
Negin <negin.khademi@gmail.com>
2024-12-19 01:21:32 +0330

    auto slide

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,20 +1,50 @@
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { list, longList, shortList } from './data';
+import { useEffect, useState } from 'react';
 
 import { FaQuoteRight } from 'react-icons/fa';
-import { useState } from 'react';
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList);
-  const pervSlide = () => {};
-  const nextSlide = () => {};
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(1);
+
+  const pervSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => {
+      clearInterval(sliderId);
+    };
+  }, [currentPerson]);
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, name, image, title, quote } = person;
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            key={id}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
+            }}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
