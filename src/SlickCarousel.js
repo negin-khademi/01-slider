034b4f23d52d1094b9b9768a3495ec47be81c9c0
Negin <negin.khademi@gmail.com>
2024-12-19 18:39:56 +0330

    change speed of slider

--- src/SlickCarousel.js
+++ src/SlickCarousel.js
@@ -9,11 +9,11 @@ const SlickCarousel = () => {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 1200,
     pauseOnHover: true,
   };
   return (
